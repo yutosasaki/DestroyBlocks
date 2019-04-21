6a2795b47b55319e26f6c7dd9450e1925addd335
yutosasaki <yuto.sasaki19@gmail.com>
2019-04-21 18:14:27 +0900

    first commit some Assets and some Library

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public GameObject masterObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        masterObj.GetComponent<GameMaster>().boxNum--;
+        Destroy(gameObject);
+    }
+}
