b052cdd251393b04db01ea2c7363eb8fc7b8482c
yutosasaki <yuto.sasaki19@gmail.com>
2019-04-21 16:00:48 +0900

    メッセージ変更

--- Assets/Scripts/KabeOut.cs
+++ Assets/Scripts/KabeOut.cs
@@ -18,6 +18,6 @@ public class KabeOut : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        GameObject.Find("Master").GetComponent<GameMaster>().GameOver("ゲーム失敗！また挑戦しよう！");
+        GameObject.Find("Master").GetComponent<GameMaster>().GameOver("ゲーム失敗！また挑戦しよう！！");
     }
 }
